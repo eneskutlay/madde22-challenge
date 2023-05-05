592d3aff145e03aba09038cef01476533e9f512d
Enes Kutlay <hi@eneskutlay.dev>
2023-05-06 02:59:32 +0300

    Update global css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,37 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import localFont from "next/font/local";
+
+const galano = localFont({
+  src: [
+    {
+      path: '../../public/galano-font/GalanoGrotesqueRegular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/galano-font/GalanoGrotesqueExtraBold.otf',
+      weight: '700',
+      style: 'extra-bold',
+    },
+    {
+      path: '../../public/galano-font/GalanoGrotesqueBold.otf',
+      weight: '400',
+      style: 'bold',
+    },
+    {
+      path: '../../public/galano-font/GalanoGrotesqueMedium.otf',
+      weight: '400',
+      style: 'medium',
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={galano.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
+
