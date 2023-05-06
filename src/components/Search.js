5ad6a6c260228cd7e2f4196e4e69106fe6a272d7
Enes Kutlay <hi@eneskutlay.dev>
2023-05-06 16:18:25 +0300

    Alt att

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 function Search() {
   return (
     <div className={style.search}>
-        <Image src="/searchIcon.svg" alt="Search" width={20} height={20} />
+        <Image src="/searchIcon.svg" alt="Search icon" width={20} height={20} />
         <input type="text" placeholder="Etkinlik Ara" />
     </div>
   );
