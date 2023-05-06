3fd15a0012d9704699487659db4f2072047712b0
Enes Kutlay <hi@eneskutlay.dev>
2023-05-06 16:18:00 +0300

    Event component dinamic

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,9 +2,7 @@ import React from "react";
 import styles from "../styles/Event.module.css";
 import Image from "next/image";
 
-function Event() {
-  let testImageUrl =
-    "https://s3-alpha-sig.figma.com/img/53a7/bebc/2548a9b67eef50835a48976e01363a25?Expires=1684108800&Signature=FbKGyBxUinSxdUi8dA-Vs6V6EVFxUPm2jKJqlkVDq-q~vjJYKtbegEsFIeI7dgXIyRxEYrR3514pzL26s8tNiQqMbNY5~L6lhSOCUZrrGe6RJE8~n-ULgQc5Vb1qfdCCc0L7mLeXIEStI1WLGxk6NrUVbK-1eQO6OkvyI4yU6z8~i8APcP8B1HNSDOJp5us~VQm5H1UmOTeMpoREfZ5PqQJfXAMsW9NjQO7kUBdAluYtiWWC5OlGoIXQqRGgN5MnOm5KWdZBxsb1zDypsKh5gWsfMhoZYBcL7pK33O0z3s73cIqTUP3uInxiW4U4Wy~RU0iSpO-cV49w1wKyA2GVyw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4";
+function Event(props) {
   return (
     <div className={styles.event}>
       <div className={styles.eventItem}>
@@ -14,26 +12,33 @@ function Event() {
           </div>
         </div>
         <div className={styles.eventItemPoster}>
-          <span className={styles.eventItemPosterFlag}>KONSER</span>
-          <Image src={testImageUrl} alt="logo" width={308} height={172} />
+          <span className={styles.eventItemPosterFlag}>{props.eventStyle}</span>
+          <Image
+            src={props.eventImageUrl}
+            alt="event image"
+            width={308}
+            height={172}
+            style={{ objectFit: "cover" }}
+          />
         </div>
         <div className={styles.eventItemCenter}>
-          <h3>BKM Yaz Etkinlikleri: Zengin Mutfağı</h3>
+          <h3>{props.eventHeader}</h3>
           <div className={styles.eventItemLocation}>
-            <Image src="/location.svg" alt="logo" width={10} height={14} />
-            <span>Maximum UNIQ Hall</span>
+            <Image
+              src="/location.svg"
+              alt="location icon"
+              width={10}
+              height={14}
+            />
+            <span>{props.eventLocation}</span>
           </div>
-          <p>
-            1978 yılında ilk kez İstanbul Şehir Tiyatrolarında bu oyunda aşçı
-            Lütfü Usta’yı canlandıran Şener Şen, 40 yıl aradan sonra aynı rolde
-            ve genç bir.... Detaylı Bilgi
-          </p>
+          <p>{props.eventDetail}</p>
         </div>
         <div className={styles.eventItemRight}>
           <button className={styles.eventItemBuyTicket}>Bilet Al</button>
           <div className={styles.celenderBox}>
             <button clas>
-              <Image src="/addIcon.svg" alt="logo" width={22} height={22} />
+              <Image src="/addIcon.svg" alt="add icon" width={22} height={22} />
             </button>
             <span>Takvime Ekle</span>
           </div>
